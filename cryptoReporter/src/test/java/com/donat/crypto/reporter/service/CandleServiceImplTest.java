364ee8cd683a42add3beb3a00b0c8150ad59407b
Donát Udvari <udvarid@hotmail.com>
2021-08-24 16:29:33 +0200

    test for candle reporter

--- cryptoReporter/src/test/java/com/donat/crypto/reporter/service/CandleServiceImplTest.java
+++ cryptoReporter/src/test/java/com/donat/crypto/reporter/service/CandleServiceImplTest.java
@@ -1,7 +1,6 @@
 package com.donat.crypto.reporter.service;
 
 import com.donat.crypto.reporter.dto.CandleDto;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,11 +22,8 @@ class CandleServiceImplTest {
     @Autowired
     CandleServiceImpl candleService;
 
-    List<CandleDto> candleDtoList;
-
-    @BeforeEach
-    void buildListOfCandleDto() {
-        candleDtoList = new ArrayList<>();
+    List<CandleDto> buildListOfCandleDtoForRecentCall() {
+        List<CandleDto> candleDtoList = new ArrayList<>();
         for (int i = 1; i <= 10; i++) {
             CandleDto candleOne = CandleDto.builder()
                     .currencyPair("USD-BTC")
@@ -52,12 +48,33 @@ class CandleServiceImplTest {
             candleDtoList.add(candleOne);
             candleDtoList.add(candleTwo);
         }
+        return candleDtoList;
     }
+
+    List<CandleDto> buildListOfCandleDtoForHistoryCall() {
+        List<CandleDto> candleDtoList = new ArrayList<>();
+        for (int i = 1; i <= 15; i++) {
+            CandleDto candleOne = CandleDto.builder()
+                    .currencyPair("USD-BTC")
+                    .close(1d)
+                    .open(1d)
+                    .count(i)
+                    .high(2d)
+                    .low(0.5d)
+                    .time(LocalDateTime.of(2021, 8, i, 0, 0))
+                    .id(1L)
+                    .build();
+            candleDtoList.add(candleOne);
+        }
+        return candleDtoList;
+    }
+
 
     @Test
     public void whenGetRecentCandlesIsCalled_thenRecentCandlesIsGiven() {
-        final CandleDto[] candleDtoArray = new CandleDto[candleDtoList.size()];
-        candleDtoList.toArray(candleDtoArray);
+        final List<CandleDto> candleDtos = buildListOfCandleDtoForRecentCall();
+        final CandleDto[] candleDtoArray = new CandleDto[candleDtos.size()];
+        candleDtos.toArray(candleDtoArray);
 
         RestTemplate mockedRestTemplate = mock(RestTemplate.class);
         when(mockedRestTemplate.getForObject("http://localhost:8080/api/candle/recent", CandleDto[].class)).thenReturn(candleDtoArray);
@@ -66,7 +83,77 @@ class CandleServiceImplTest {
         final List<CandleDto> recentCandles = candleService.getRecentCandles();
 
         assertThat(recentCandles.size()).isEqualTo(20);
+    }
 
+    @Test
+    public void whenGetCandleListIsCalledWithPeriodLenghtOne_thenAppropriateCandlesIsGiven() {
+        final String CURRENCY = "USD/BTC";
+        final int PERIOD_LENGTH = 1;
+        final int NUMBER_OF_CANDLES = 20;
+
+        final List<CandleDto> candleDtos = buildListOfCandleDtoForHistoryCall();
+        final CandleDto[] candleDtoArray = new CandleDto[candleDtos.size()];
+        candleDtos.toArray(candleDtoArray);
+
+        RestTemplate mockedRestTemplate = mock(RestTemplate.class);
+        when(mockedRestTemplate.getForObject("http://localhost:8080/api/candle/list/" + CURRENCY + "/" + PERIOD_LENGTH * NUMBER_OF_CANDLES,
+                CandleDto[].class)).thenReturn(candleDtoArray);
+        candleService.setRestTemplate(mockedRestTemplate);
+
+        final List<CandleDto> recentCandles = candleService.getCandleList(CURRENCY, PERIOD_LENGTH, NUMBER_OF_CANDLES);
+
+        assertThat(recentCandles.size()).isEqualTo(15);
+        for (int i = 1; i <= 15; i++) {
+            assertThat(recentCandles.get(i - 1).getCount()).isEqualTo(i);
+        }
+
+    }
+
+
+    @Test
+    public void whenGetCandleListIsCalledWithPeriodLenghtTwo_thenAppropriateCandlesIsGiven() {
+        final String CURRENCY = "USD/BTC";
+        final int PERIOD_LENGTH = 2;
+        final int NUMBER_OF_CANDLES = 20;
+
+        final List<CandleDto> candleDtos = buildListOfCandleDtoForHistoryCall();
+        final CandleDto[] candleDtoArray = new CandleDto[candleDtos.size()];
+        candleDtos.toArray(candleDtoArray);
+
+        RestTemplate mockedRestTemplate = mock(RestTemplate.class);
+        when(mockedRestTemplate.getForObject("http://localhost:8080/api/candle/list/" + CURRENCY + "/" + PERIOD_LENGTH * NUMBER_OF_CANDLES,
+                CandleDto[].class)).thenReturn(candleDtoArray);
+        candleService.setRestTemplate(mockedRestTemplate);
+
+        final List<CandleDto> recentCandles = candleService.getCandleList(CURRENCY, PERIOD_LENGTH, NUMBER_OF_CANDLES);
+
+        assertThat(recentCandles.size()).isEqualTo(7);
+        for (int i = 1; i <= 7; i++) {
+            assertThat(recentCandles.get(i - 1).getCount()).isEqualTo(i * 4 + 1);
+        }
+    }
+
+    @Test
+    public void whenGetCandleListIsCalledWithPeriodLenghtFour_thenAppropriateCandlesIsGiven() {
+        final String CURRENCY = "USD/BTC";
+        final int PERIOD_LENGTH = 4;
+        final int NUMBER_OF_CANDLES = 20;
+
+        final List<CandleDto> candleDtos = buildListOfCandleDtoForHistoryCall();
+        final CandleDto[] candleDtoArray = new CandleDto[candleDtos.size()];
+        candleDtos.toArray(candleDtoArray);
+
+        RestTemplate mockedRestTemplate = mock(RestTemplate.class);
+        when(mockedRestTemplate.getForObject("http://localhost:8080/api/candle/list/" + CURRENCY + "/" + PERIOD_LENGTH * NUMBER_OF_CANDLES,
+                CandleDto[].class)).thenReturn(candleDtoArray);
+        candleService.setRestTemplate(mockedRestTemplate);
+
+        final List<CandleDto> recentCandles = candleService.getCandleList(CURRENCY, PERIOD_LENGTH, NUMBER_OF_CANDLES);
+
+        assertThat(recentCandles.size()).isEqualTo(3);
+        assertThat(recentCandles.get(0).getCount()).isEqualTo(10 + 12);
+        assertThat(recentCandles.get(1).getCount()).isEqualTo(26 + 12);
+        assertThat(recentCandles.get(2).getCount()).isEqualTo(42 + 12);
     }
 
 }
